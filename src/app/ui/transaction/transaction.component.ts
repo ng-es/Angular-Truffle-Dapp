d29813f9ecd87b243e47f8bba7d0ffe1e08e18c2
AntonioCardenas <antonioalx66@gmail.com>
2019-03-30 18:39:42 -0600

    Rugular expretions validators

--- src/app/ui/transaction/transaction.component.ts
+++ src/app/ui/transaction/transaction.component.ts
@@ -2,7 +2,9 @@ import { contract } from 'truffle-contract';
 import { Component, OnInit } from '@angular/core';
 import { ContractService } from './../../services/contract/contract.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-type TransactionField = 'sendadress' | 'amount';
+import { MdcSnackbar } from '@angular-mdc/web';
+
+type TransactionField = 'sendaddress' | 'amount';
 type FormErrors = {[u in TransactionField]: string};
 @Component({
   selector: 'app-transaction',
@@ -11,20 +13,21 @@ type FormErrors = {[u in TransactionField]: string};
 })
 export class TransactionComponent implements OnInit {
   direction: string;
-  adress: string;
+  address: string;
   amount: string;
   balance: string;
   success: boolean;
   transactionDone: boolean;
+
   transactionForm: FormGroup;
   formErrors: FormErrors = {
-    sendadress: '',
+    sendaddress: '',
     amount: '',
   };
   validationMessages = {
-   sendadress: {
-   required: 'The send adress is required ',
-   pattern: 'that´s no looks like a valid adress',
+   sendaddress: {
+   required: 'The send address is required ',
+   pattern: 'that´s no looks like a valid address',
    minlength: 'a address must have much than 40 characters',
 
    },
@@ -35,7 +38,7 @@ export class TransactionComponent implements OnInit {
   };
 
 // tslint:disable-next-line: no-shadowed-variable
-  constructor(private frb: FormBuilder, private contract: ContractService) {
+  constructor(private frb: FormBuilder, private contract: ContractService, private snackbar: MdcSnackbar) {
     contract.seeAccountInfo().then((value: any) => {
     this.direction = value.originAccount;
     this.balance = value.balance;
@@ -47,13 +50,13 @@ export class TransactionComponent implements OnInit {
   }
   buildForm() {
     this.transactionForm = this.frb.group({
-      sendadress: ['', [
+      sendaddress: ['', [
       Validators.required,
       Validators.minLength(42),
        ]],
       amount : ['', [
         Validators.required,
-        Validators.pattern('(.[0-9])'),
+        Validators.pattern(/^[+-]?\d+(\.\d+)?$/),
      ]],
     });
     this.transactionForm.valueChanges.subscribe((data) => this.onValueChanged(data));
@@ -64,30 +67,45 @@ export class TransactionComponent implements OnInit {
   }
 
   transferEth(e) {
-  this.adress = this.transactionForm.value.sendadress;
+  this.address = this.transactionForm.value.sendaddress;
   this.amount = this.transactionForm.value.amount;
 
-  this.contract.trasnferEther(this.direction, this.adress, this.amount).then((r) => {
+  this.contract.trasnferEther(this.direction, this.address, this.amount).then((r) => {
     this.showMessage(true, 5000);
   }).catch((e) => {
     this.showMessage(false, 5000);
+    this.failure();
   });
   }
 
   showMessage(exito, duracion) {
     this.transactionDone = true;
     this.success = exito;
+    this.succes();
     setTimeout(() => {
         this.success = !exito;
         this.transactionDone = false;
     }, duracion);
   }
+  failure() {
+    const snackbarRef = this.snackbar.open('Transaction failed.');
+    snackbarRef.afterDismiss().subscribe(reason => {
+    });
+  }
+
+
+  succes() {
+    const snackbarRef = this.snackbar.open('Transaction complete successfuly.');
+    snackbarRef.afterDismiss().subscribe(reason => {
+    });
+  }
+
 
   onValueChanged(data?: any) {
     if (!this.transactionForm) { return; }
     const form = this.transactionForm;
     for (const field in this.formErrors) {
-      if (Object.prototype.hasOwnProperty.call(this.formErrors, field) && (field === 'sendadress' || field === 'amount')) {
+      if (Object.prototype.hasOwnProperty.call(this.formErrors, field) && (field === 'sendaddress' || field === 'amount')) {
         this.formErrors[field] = '';
         const control = form.get(field);
         if (control && control.dirty && !control.valid) {
