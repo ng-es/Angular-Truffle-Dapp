7d39fd68ec30db25cfb4500ebbb84901d747fb38
Antonio Cardenas <antonioalx66@gmail.com>
2019-06-01 21:31:45 -0600

    Now you can build in production mode and use the deployed contract  and get a identicon soon will we can use ipfs

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,22 @@
+require("dotenv").config();
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic = process.env.MNENOMIC;
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
+    },
+    rinkeby: {
+      // must be a thunk, otherwise truffle commands may hang in CI
+      provider: () =>
+        new HDWalletProvider(mnemonic, process.env.YOUR_RINKEBY_TEST_URL
+          ),
+      network_id: "4",
+      skipDryRun: true
     }
   },
   compilers: {
