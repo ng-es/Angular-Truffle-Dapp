e14758db1c7d1f1370dd62018131af7ed5db503d
Antonio Cardenas <antonioalx66@gmail.com>
2019-09-03 12:51:56 -0600

    UI and mobile optimization

--- Frontend/src/app/ui/ui.routes.ts
+++ Frontend/src/app/ui/ui.routes.ts
@@ -3,7 +3,8 @@ import { HomeComponent } from './home/home.component';
 import { TransactionComponent } from './transaction/transaction.component';
 import { Routes } from '@angular/router';
 export const UiRoute: Routes = [
-  { path: '', component: TransactionComponent },
+  { path: '', redirectTo: 'money', pathMatch: 'full'},
+  { path: 'money', component: TransactionComponent },
   { path: 'home', component: HomeComponent},
   { path: 'account', component: AccountComponent}
 ];
