6c4ba6f33e40fd0f469bd379ad8aa0f7fd610aed
Antonio Cardenas <antonioalx66@gmail.com>
2019-10-28 16:47:18 -0600

    new web3 version to stable version

--- Frontend/src/app/services/contract/contract.service.ts
+++ Frontend/src/app/services/contract/contract.service.ts
@@ -13,60 +13,54 @@ declare let window: any;
 })
 
 export class ContractService {
-  private web3Provider: null;
-  private contracts: {};
-  public compatible: boolean;
+  private web3Provider: null;  
   private accounts: string[];
   public accountsObservable = new Subject<string[]>();
+  public compatible: boolean;
 
   constructor(private snackbar: MdcSnackbar) {
     if (typeof window.web3 === 'undefined' || (typeof window.ethereum !== 'undefined')) {
       this.web3Provider = window.ethereum || window.web3;
-      console.log(this.web3Provider);
       window.web3 = new Web3(this.web3Provider);
     } else {
-      this.web3Provider = new Web3(new Web3.providers.HttpProvider('HTTP://127.0.0.1:7545'));
-      // if you are using linix or ganche cli maybe the port is  http://localhost:8545
+      this.web3Provider = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
+      // if you are using linux or ganche cli maybe the port is  http://localhost:8545
       //   Web3.providers.HttpProvider.prototype.sendAsync = Web3.providers.HttpProvider.prototype.send;
       //   this.web3Provider = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/Private_key'));
       // Change with your credentials as the test network and private key in infura.io
-       }
-     }
-
-
-
-
+    }
+  }
 
   seeAccountInfo() {
     return new Promise((resolve, reject) => {
       window.web3.eth.getCoinbase((err, account) => {
-          if (account === true) {
-            console.log('dondt work' + account);
-            return reject({name: 'account'});
-          } else {
-            window.web3.eth.getBalance(account, (error, balance) => {
-              if (error === false) {
-                return resolve({
-                  originAccount: account,
-                  balance: (window.web3.utils.fromWei(balance, 'ether'))
-                });
-              } else {
-                console.log(balance);
-                return reject({name: 'balance'});
-              }
-            });
-          }
+        if (account === true) {
+          console.log('dondt work' + account);
+          return reject({ name: 'account' });
+        } else {
+          window.web3.eth.getBalance(account, (error, balance) => {
+            if (error === null) {
+              return resolve({
+                originAccount: account,
+                balance: (window.web3.utils.fromWei(balance, 'ether'))
+              });
+            } else {
+              console.log(balance);
+              return reject({ name: 'balance' });
+            }
+          });
+        }
       });
     });
   }
 
-   refreshAccounts() {
+  refreshAccounts() {
 
     window.web3.eth.getAccounts((err, accs) => {
       console.log('Refreshing accounts');
       if (err === true) {
         console.warn('There was an error fetching your accounts.');
-        console.log(err , accs);
+        console.log(err, accs);
         return;
       }
 
@@ -84,7 +78,7 @@ export class ContractService {
       }
 
       console.log('ready');
-   });
+    });
   }
 
   trasnferEther(originAccount, destinyAccount, amount) {
@@ -95,31 +89,31 @@ export class ContractService {
       paymentContract.setProvider(that.web3Provider);
 
       paymentContract.deployed().then((instance) => {
-          return instance.nuevaTransaccion(
-            destinyAccount,
-            {
-              from: originAccount,
-              value: window.web3.utils.toWei(amount, 'ether')
-            });
-        }).then((status) => {
-          if (status) {
-            return resolve({status: true});
-          }
-        }).catch((error) => {
-          console.log(error);
+        return instance.nuevaTransaccion(
+          destinyAccount,
+          {
+            from: originAccount,
+            value: window.web3.utils.toWei(amount, 'ether')
+          });
+      }).then((status) => {
+        if (status) {
+          return resolve({ status: true });
+        }
+      }).catch((error) => {
+        console.log(error);
 
-          return reject('Error transfering Ether');
-        });
+        return reject('Error transfering Ether');
+      });
     });
   }
 
   failure(message: string) {
     const snackbarRef = this.snackbar.open(message);
-    snackbarRef.afterDismiss().subscribe(reason => {});
+    snackbarRef.afterDismiss().subscribe(reason => { });
   }
 
   succes() {
     const snackbarRef = this.snackbar.open('Transaction complete successfuly');
-    snackbarRef.afterDismiss().subscribe(reason => {});
+    snackbarRef.afterDismiss().subscribe(reason => { });
   }
 }
