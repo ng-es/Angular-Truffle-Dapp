c636af35be11a39cd1c7d7b42c2922094a6e13d2
antoniocardenas <antonioalx66@gmail.com>
2021-02-24 18:31:49 -0600

    feat: add material design

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppMaterialModule} from "./app-material.module";
+import { AppRoutingModule } from './app-routing.module';
+import {UiModule} from "./ui/ui.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    AppMaterialModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    UiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Frontend/src/app/ui/ui.module.ts
+++ Frontend/src/app/ui/ui.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+
+// Components
 import { AccountComponent } from './account/account.component';
 import { HomeComponent } from './home/home.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
 import { TransactionComponent } from './transaction/transaction.component';
 import { ErrorComponent } from './error/error.component';
+import { AppMaterialModule } from "../app-material.module";
 
 // Routing
 import { UiRoute} from "./ui.routes";
@@ -22,7 +26,8 @@ import { RouterModule} from "@angular/router";
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(UiRoute)
+    RouterModule.forChild(UiRoute),
+    AppMaterialModule
   ],
   exports: [
     TopNavComponent,
